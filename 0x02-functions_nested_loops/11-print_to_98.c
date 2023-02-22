a6020bf2230fba48dbc1f9f62aa007d421c0a117
asparuah <paruah.hawk@gmail.com>
2023-02-21 16:25:16 -0800

    twelve

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,8 +3,8 @@
 #include <stdio.h>
 
 /**
- * print_to_98 - Prints all natural nubers from input to 98,
- *               in order seperated by a comma followed by a space.
+ * print_to_98 - Prints all natural nubers from input to 98, in other to seperate by a comma followed by a space.
+ *              
  * @n: The number to begin counting at. 
  */
 void print_to_98(int n)
