9decd679ac60a7b37cd3e9e25588235aed0179b0
asparuah <paruah.hawk@gmail.com>
2023-03-16 16:31:50 -0700

    six

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,39 +1,46 @@
+#include <stdlib.h>
+#include<stdio.h>
 #include "main.h"
 
 /**
- * _realloc - reallocates a memory block
- * @ptr: pointer to previous memory block
- * @old_size: size in bytes of allocated space for `ptr`
- * @new_size: size in bytes for new allocated space
- * Return: Pointer to new memory block, or NULL if error
+ * _realloc - allocate memory and set all values to 0
+ * @ptr: pointer to the memory previously allocated (malloc(old_size))
+ * @old_size: size previously allocated
+ * @new_size: new size to reallocate
+ * Return: pointer to reallocated memory
  */
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *p, *copy;
+	void *p;
 	unsigned int i;
 
-	if (new_size == old_size)
-		return (ptr);
-	if (ptr != NULL && new_size == 0)
+	if (new_size == 0 && ptr != NULL) /* free memory if reallocate 0 */
 	{
 		free(ptr);
 		return (NULL);
 	}
-	if (ptr == NULL)
+
+	if (new_size == old_size) /* return ptr if reallocating same old size */
+		return (ptr);
+
+	if (ptr == NULL) /* malloc new size if ptr is originally null */
 	{
 		p = malloc(new_size);
 		if (p == NULL)
 			return (NULL);
-		return (p);
+		else
+			return (p);
 	}
 
-	p = malloc(new_size);
+	p = malloc(new_size); /* malloc and check error */
 	if (p == NULL)
 		return (NULL);
-	copy = ptr;
-	for (i = 0; i < old_size; i++)
-		p[i] = copy[i];
-	free(ptr);
+
+	/* fill up values up till minimum of old or new size */
+	for (i = 0; i < old_size && i < new_size; i++)
+		*((char *)p + i) = *((char *)ptr + i);
+	free(ptr); /* free old ptr */
+
 	return (p);
 }
